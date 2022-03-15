bc9f0159050255252eb385f394de56bb46e0a14b
hrishikeshtele <hrishikesh.tele0@gmail.com>
2022-03-14 17:38:46 -0700

    Added read write logic for file

--- Assignments/Assignment 3/client.py
+++ Assignments/Assignment 3/client.py
@@ -4,21 +4,30 @@ import payload_pb2
 import payload_pb2_grpc
 import pyspeedtest
 
+PAYLOAD_SIZE = 1024 * 1024
 
-def run():
+
+def run(file_path: str):
     speed = pyspeedtest.SpeedTest("www.google.com")
     print(speed.download())
+    input_file = open(file_path, 'rb')
+    data = input_file.read(PAYLOAD_SIZE)
+    while len(data) > 0:
+        send_request(data, filename=file_path.split("/")[-1])
+        data = input_file.read(PAYLOAD_SIZE)
+    send_request(data, filename=file_path.split("/")[-1])
+    input_file.close()
 
 
-def send_request(data):
+def send_request(data, filename):
     channel = grpc.insecure_channel('localhost:50051')
     # create a stub (client)
     stub = payload_pb2_grpc.SendAdaptivePayloadStub(channel)
     try:
         # create a valid request
-        request = payload_pb2.Request(data=data, filename="test.csv")
+        request = payload_pb2.Request(filename=filename, data=data)
         # make api call
-        response = stub.execute_query(request)
+        response = stub.data_transfer(request)
         print(response.status)
     except KeyboardInterrupt:
         channel.unsubscribe(close)
@@ -30,4 +39,4 @@ def close(channel):
 
 
 if __name__ == "__main__":
-    run()
+    run("data/input/sample_data.csv")
--- Assignments/Assignment 3/server.py
+++ Assignments/Assignment 3/server.py
@@ -7,10 +7,29 @@ import payload_pb2_grpc
 
 
 class Listener(payload_pb2_grpc.SendAdaptivePayloadServicer):
+    MAXIMUM_SIZE_ALLOWED = 1024 * 1024 * 10
+    data_dic = {}
 
     def data_transfer(self, request, context):
-
+        file_name = request.filename
+        input_bytes = request.data
+        # print("Received " + str(len(input_bytes)))
+        if len(input_bytes) == 0:
+            self.write_file(file_name)
+        else:
+            if file_name not in self.data_dic:
+                self.data_dic[file_name] = input_bytes
+            else:
+                self.data_dic[file_name] = self.data_dic[file_name] + input_bytes
+            if len(self.data_dic[file_name]) > self.MAXIMUM_SIZE_ALLOWED:
+                self.write_file(file_name)
+                self.data_dic.pop(file_name)
         return payload_pb2.Response(status=True)
+
+    def write_file(self, file_name):
+        output_file = open("data/output/" + file_name, "ab+")
+        output_file.write(self.data_dic[file_name])
+        output_file.close()
 
 
 def serve():
